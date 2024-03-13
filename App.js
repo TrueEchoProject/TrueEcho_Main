ddf95277e472b5bd76cc13fe726c27c0b610bea1
HolMoly <ryan10920@naver.com>
2024-03-13 16:31:09 +0900

    Feature: 하단 네비게이션 바 생성 각 화면별 이동용 하단 네비게이션 바

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Navigation from "./navigations"; //네비게이션 폴더
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation /> //하단 Tab 네비게이터 생성
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
