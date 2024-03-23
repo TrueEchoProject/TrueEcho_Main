2443770c6291406901d0ca914c5d63368479e7c4
HolMoly <ryan10920@naver.com>
2024-03-24 04:25:12 +0900

    Feat: 현재 탭의 route 파악 및 이에 따른 버튼 변화

--- components/FeedButton.js
+++ components/FeedButton.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
 
-// isSelected prop을 추가합니다.
-const FeedButton = ({ title, onPress }) => {
+const FeedButton = ({ title, onPress, isSelected }) => {
 	return (
-		<TouchableOpacity onPress={onPress} style={styles.button}>
+		<TouchableOpacity onPress={onPress} style={[styles.button, isSelected && styles.selected]}>
 			<View>
-				<Text style={styles.text}>{title}</Text>
+				<Text style={[styles.text, isSelected && styles.selectedText]}>{title}</Text>
 			</View>
 		</TouchableOpacity>
 	);
@@ -16,11 +15,20 @@ const styles = StyleSheet.create({
 	button: {
 		marginRight: 10,
 		marginLeft: 10,
+		padding: 5,
+    borderRadius: 5,
+    height: 30,
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
 	text: {
 		color: 'black',
+	},
+	selected: {
+		backgroundColor: '#3B4664', // 예시로 선택됐을 때 배경색을 변경
+	},
+	selectedText: {
+		color: 'white', // 예시로 선택됐을 때 텍스트 색상을 변경
 	},
 });
 
--- navigations/AppTabNavigator/MainFeedTab.js
+++ navigations/AppTabNavigator/MainFeedTab.js
@@ -7,37 +7,39 @@ import OtherFeed from './FeedTabNavigator/OtherFeed';
 import FeedButton from "../../components/FeedButton";
 
 const MainFeedTab = createBottomTabNavigator();
-
-export const MainFeedTabScreen = () => { // 메인 피드 속 Stack 구성
-	
+export const MainFeedTabScreen = () => {
 	return (
 		<MainFeedTab.Navigator
 			screenOptions={{
 				tabBarStyle: { display: 'none' } // 하단 탭 바 숨김
 			}}
+			initialRouteName="FriendFeed" // 최초 어플 실행 시 FriendFeed가 기본 화면으로 설정
 		>
 			<MainFeedTab.Screen
 				name="FriendFeed"
 				component={FriendFeed}
-				options={({ navigation }) => ({ // 메인 피드 화면에서 상단 네비 구현
-					headerTitle: () => (
-						<View style={{
-							flexDirection: 'row',
-						}}>
-							<FeedButton
-								title="친구"
-								onPress={() => navigation.navigate('FriendFeed')}
-							/>
-							<FeedButton
-								title="친구의 친구"
-								onPress={() => navigation.navigate('FofFeed')}
-							/>
-							<FeedButton
-								title="더보기"
-								onPress={() => navigation.navigate('OtherFeed')}
-							/>
-						</View>
-					),
+				options={({ navigation }) => ({
+					headerTitle: () => {
+						// 현재 활성화된 탭 이름 가져오기
+						const currentRouteName = navigation.getState().routes[navigation.getState().index].name;
+						return (
+							<View style={{ flexDirection: 'row' }}>
+								<FeedButton
+									title="친구"
+									onPress={() => navigation.navigate('FriendFeed')}
+									isSelected={currentRouteName === 'FriendFeed'}
+								/>
+								<FeedButton
+									title="친구의 친구"
+									onPress={() => navigation.navigate('FofFeed')}
+								/>
+								<FeedButton
+									title="더보기"
+									onPress={() => navigation.navigate('OtherFeed')}
+								/>
+							</View>
+						);
+					},
 					headerTitleAlign: "center",
 					headerStyle: {
 						height: 30,
@@ -48,25 +50,28 @@ export const MainFeedTabScreen = () => { // 메인 피드 속 Stack 구성
 			<MainFeedTab.Screen
 				name="FofFeed"
 				component={FofFeed}
-				options={({ navigation }) => ({ // 메인 피드 화면에서 상단 네비 구현
-					headerTitle: () => (
-						<View style={{
-							flexDirection: 'row',
-						}}>
-							<FeedButton
-								title="친구"
-								onPress={() => navigation.navigate('FriendFeed')}
-							/>
-							<FeedButton
-								title="친구의 친구"
-								onPress={() => navigation.navigate('FofFeed')}
-							/>
-							<FeedButton
-								title="더보기"
-								onPress={() => navigation.navigate('OtherFeed')}
-							/>
-						</View>
-					),
+				options={({ navigation }) => ({
+					headerTitle: () => {
+						// 현재 활성화된 탭 이름 가져오기
+						const currentRouteName = navigation.getState().routes[navigation.getState().index].name;
+						return (
+							<View style={{ flexDirection: 'row' }}>
+								<FeedButton
+									title="친구"
+									onPress={() => navigation.navigate('FriendFeed')}
+								/>
+								<FeedButton
+									title="친구의 친구"
+									onPress={() => navigation.navigate('FofFeed')}
+									isSelected={currentRouteName === 'FofFeed'}
+								/>
+								<FeedButton
+									title="더보기"
+									onPress={() => navigation.navigate('OtherFeed')}
+								/>
+							</View>
+						);
+					},
 					headerTitleAlign: "center",
 					headerStyle: {
 						height: 30,
@@ -77,25 +82,28 @@ export const MainFeedTabScreen = () => { // 메인 피드 속 Stack 구성
 			<MainFeedTab.Screen
 				name="OtherFeed"
 				component={OtherFeed}
-				options={({ navigation }) => ({ // 메인 피드 화면에서 상단 네비 구현
-					headerTitle: () => (
-						<View style={{
-							flexDirection: 'row',
-						}}>
-							<FeedButton
-								title="친구"
-								onPress={() => navigation.navigate('FriendFeed')}
-							/>
-							<FeedButton
-								title="친구의 친구"
-								onPress={() => navigation.navigate('FofFeed')}
-							/>
-							<FeedButton
-								title="더보기"
-								onPress={() => navigation.navigate('OtherFeed')}
-							/>
-						</View>
-					),
+				options={({ navigation }) => ({
+					headerTitle: () => {
+						// 현재 활성화된 탭 이름 가져오기
+						const currentRouteName = navigation.getState().routes[navigation.getState().index].name;
+						return (
+							<View style={{ flexDirection: 'row' }}>
+								<FeedButton
+									title="친구"
+									onPress={() => navigation.navigate('FriendFeed')}
+								/>
+								<FeedButton
+									title="친구의 친구"
+									onPress={() => navigation.navigate('FofFeed')}
+								/>
+								<FeedButton
+									title="더보기"
+									onPress={() => navigation.navigate('OtherFeed')}
+									isSelected={currentRouteName === 'OtherFeed'}
+								/>
+							</View>
+						);
+					},
 					headerTitleAlign: "center",
 					headerStyle: {
 						height: 30,
