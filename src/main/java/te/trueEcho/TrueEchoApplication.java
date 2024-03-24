5d9ee548e84dd3876b1ee952c22a2b4afb77a5f4
HeeJohn <gjshj366@gmail.com>
2024-03-24 13:39:20 +0900

    chore : 프로젝트 생성

---
+++ src/main/java/te/trueEcho/TrueEchoApplication.java
@@ -0,0 +1,13 @@
+package te.trueEcho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueEchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueEchoApplication.class, args);
+	}
+
+}
