5cab13d1563591808b913b6465b19729e299b4cf
HolMoly <ryan10920@naver.com>
2024-03-26 19:03:23 +0900

    Feat: 이미지 하나인 경우, 단일 Image 렌더링

--- components/ImageButton.js
+++ components/ImageButton.js
@@ -1,50 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { Image, TouchableOpacity, View } from 'react-native';
 
-export const ImageButton = ({ author, images }) => { // images prop 추가
+export const ImageButton = ({ author, images }) => {
 	const [imageIndex, setImageIndex] = useState(0);
 	
 	const changeImage = () => {
-		setImageIndex(prevIndex => (prevIndex + 1) % images.length);
+		if (images.length > 1) {
+			setImageIndex(prevIndex => (prevIndex + 1) % images.length);
+		}
 	};
 	
 	// 기본 이미지 혹은 대체 이미지 설정
-	const defaultImages = [
-		"https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=150&h=150&fit=crop",
-		"https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?w=450&h=450&fit=crop"
-	];
+	const defaultImage = "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?w=450&h=450&fit=crop";
 	
 	// 이미지 배열이 비어있지 않으면 사용, 그렇지 않으면 기본 이미지 사용
-	const firstImageUri = images.length > 0 ? images[0] : defaultImages[0];
-	const secondImageUri = images.length > 1 ? images[1] : firstImageUri;
+	const imageUri = images.length > 0 ? images[Math.min(imageIndex, images.length - 1)] : defaultImage;
 	
 	return (
 		<View style={{ position: 'relative' }}>
-			<TouchableOpacity onPress={() => changeImage()} style={{ zIndex: 2, position: 'absolute', top: 10, left: 10 }}>
+			{images.length > 1 && (
+				<TouchableOpacity onPress={changeImage} style={{ zIndex: 2, position: 'absolute', top: 10, left: 10 }}>
+					<Image
+						source={{ uri: images[(imageIndex + 1) % images.length] }}
+						style={{
+							borderColor: '#ffffff',
+							borderWidth: 2,
+							height: 150,
+							width: 100,
+						}}
+					/>
+				</TouchableOpacity>
+			)}
+			<TouchableOpacity onPress={changeImage} style={{ zIndex: 1 }}>
 				<Image
-					source={{ uri: imageIndex === 0 ? firstImageUri : secondImageUri }}
-					style={{
-						borderColor: '#ffffff',
-						borderWidth: 2,
-						height: 150,
-						width: 100,
-						resizeMode: 'stretch',
-					}}
-				/>
-			</TouchableOpacity>
-			<TouchableOpacity onPress={() => changeImage()} style={{ zIndex: 1 }}>
-				<Image
-					source={{ uri: imageIndex === 0 ? secondImageUri : firstImageUri }}
+					source={{ uri: imageUri }}
 					style={{
 						height: 480,
 						width: '100%',
-						resizeMode: 'stretch'
 					}}
 				/>
 			</TouchableOpacity>
 		</View>
 	);
 };
+
 
 
 
