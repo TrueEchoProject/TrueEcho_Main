0a1428264605b97630913bca85b1cf8a6989455a
HolMoly <ryan10920@naver.com>
2024-03-28 02:09:03 +0900

    Feat: ... 버튼 시, modal

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -1,14 +1,24 @@
-import React, { PureComponent } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Share } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Share } from 'react-native';
 import { Image } from 'expo-image';
 import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 import { ImageButton } from "./ImageButton"; // 경로 확인 필요
 
-export default class CardComponent extends PureComponent {
-	render() {
-		const { data } = this.props;
-		
-		return (
+export default function CardComponent({ data }) {
+	const [isOptionsVisible, setIsOptionsVisible] = useState(false);
+	const [buttonLayout, setButtonLayout] = useState({ x: 0, y: 0, width: 0, height: 0 });
+	
+	const toggleOptionsVisibility = () => {
+		setIsOptionsVisible(!isOptionsVisible);
+	};
+	
+	const hideOptions = () => {
+		if (isOptionsVisible) {
+			setIsOptionsVisible(false);
+		}
+	};
+	return (
+		<TouchableWithoutFeedback onPress={hideOptions}>
 			<View style={styles.cardContainer}>
 				<View style={styles.cardItem}>
 					<View style={styles.left}>
@@ -21,8 +31,27 @@ export default class CardComponent extends PureComponent {
 							<Text note>{new Date(data.created).toDateString()}</Text>
 						</View>
 					</View>
+					<TouchableOpacity style={styles.right} onPress={toggleOptionsVisibility} onLayout={(event) => {
+						const layout = event.nativeEvent.layout;
+						setButtonLayout(layout);
+					}}>
+						<Text style={{fontSize: 30}}>...</Text>
+					</TouchableOpacity>
 				</View>
 				<ImageButton images={data.images} />
+				{isOptionsVisible && (
+					<View style={[
+						styles.optionsContainer,
+						{
+							top: buttonLayout.y + buttonLayout.height, // 버튼의 하단에 위치
+							right: 0 // 화면의 오른쪽 끝에 위치
+						}
+					]}>
+						<Text style={styles.optionItem}>옵션 1</Text>
+						<Text style={styles.optionItem}>옵션 2</Text>
+						{/* 기타 옵션들 */}
+					</View>
+				)}
 				<View style={styles.cardItem}>
 					<Text style={styles.title}>{data.title.slice(0, 15)}</Text>
 				</View>
@@ -45,11 +74,28 @@ export default class CardComponent extends PureComponent {
 					</View>
 				</View>
 			</View>
+		</TouchableWithoutFeedback>
 		);
 	}
-}
 
 const styles = StyleSheet.create({
+	optionsContainer: {
+		position: 'absolute',
+		zIndex: 3,
+		backgroundColor: 'white',
+		padding: 8,
+		borderRadius: 4,
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 4,
+		shadowOpacity: 0.3,
+		elevation: 4,
+		marginTop: 10, // '...' 버튼과 옵션 컨테이너 사이의 간격을 조정하세요.
+	},
+	optionItem: {
+		paddingVertical: 8,
+		paddingHorizontal: 16,
+	},
 	cardContainer: {
 		marginBottom: 16,
 	},
