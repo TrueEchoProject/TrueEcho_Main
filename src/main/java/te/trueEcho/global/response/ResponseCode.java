9047aeafdb35cfce4ae7f96a21e2f06cf118edd7
HeeJohn <gjshj366@gmail.com>
2024-03-29 11:20:49 +0900

    feat. 응답 폼 및 에러코드 지정

---
+++ src/main/java/te/trueEcho/global/response/ResponseCode.java
@@ -0,0 +1,34 @@
+package te.trueEcho.global.response;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/*
+ * Response Convention
+ * - 도메인 별로 나누어 관리
+ * - [동사_목적어_SUCCESS] 형태로 생성
+ * - 코드는 도메인명 앞에서부터 1~2글자로 사용
+ * - 메시지는 "~니다."로 마무리
+ */
+
+
+@Getter
+@AllArgsConstructor
+public enum ResponseCode {
+
+    // user
+    REGISTER_SUCCESS(200, "U001", "회원가입에 성공했습니다.."),
+    LOGIN_SUCCESS(200, "U002", "로그인에 성공했습니다."),
+    DUPLICATED_ACCOUNT_GOOD(200, "U002", "중복되지 않은 계정입니다."),
+    DUPLICATED_ACCOUNT_BAD(200, "U001", "중복된 계정입니다."),
+    VERIFY_EMAIL_SUCESS(200, "U002", "이메일 인증에 성공했습니다."),
+    VERIFY_EMAIL_FAIL(200, "U003","이메일 인증에 실패했습니다."),
+    SEND_EMAIL_SUCCESS(200, "U004", "이메일을 성공적으로 전송했습니다."),
+    SEND_EMAIL_FAIL(200, "U005", "이메일 전송에 실패했습니다.");
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
