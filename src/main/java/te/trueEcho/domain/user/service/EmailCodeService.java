079ebd46ead9f421d8b16a1e7b48073d99c72625
HeeJohn <gjshj366@gmail.com>
2024-03-29 13:22:24 +0900

    refactor : 이메일 전송코드 메모리 저장

--- src/main/java/te/trueEcho/domain/user/service/EmailCodeService.java
+++ src/main/java/te/trueEcho/domain/user/service/EmailCodeService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import te.trueEcho.domain.user.dto.EmailUserDto;
+import te.trueEcho.domain.user.repository.EmailMemoryRepository;
 import te.trueEcho.domain.user.repository.UserRepository;
 import te.trueEcho.infra.email.EmailService;
 
@@ -24,31 +25,30 @@ public class EmailCodeService {
     private static final String REGISTER_EMAIL_SUBJECT_POSTFIX = " 님의 진실된 모습을 공유해주세요!";
     private static final String RESET_PASSWORD_EMAIL_SUBJECT_POSTFIX = " 님, 계정의 비밀번호를 복구해주세요";
 
-    private final UserRepository userRepository;
+    private final EmailMemoryRepository emailMemoryRepository;
 
     private final EmailService emailService;
     private String confirmEmailUI;
 
     public void sendRegisterCode(EmailUserDto emailUserDto) {
-        final String code = generateRandomCode();
-        emailService.sendHtmlTextEmail(emailUserDto.getUsername() +
-                        REGISTER_EMAIL_SUBJECT_POSTFIX, getRegisterEmailText(emailUserDto.getEmail(), code),
-                emailUserDto.getEmail());
+        final String code = generateRandomCode(); // 랜덤 번호 생성
 
-        userRepository.saveCheckCode(emailUserDto.getEmail(),code);
+        emailService.sendHtmlTextEmail(
+                emailUserDto.getUsername() + REGISTER_EMAIL_SUBJECT_POSTFIX,
+                getRegisterEmailText(emailUserDto.getEmail(), code),
+                emailUserDto.getEmail()); // 메일 전송
+
+        emailMemoryRepository.saveCheckCode(emailUserDto.getEmail(),code); // 랜덤 번호/이메일 저장.
+    }
+
+    public void deleteCheckCode(EmailUserDto emailUserDto){
+        emailMemoryRepository.deleteCheckCode(emailUserDto.getEmail());
     }
 
     public boolean checkRegisterCode( String email, String code) {
-        final String savedCode = userRepository.findCheckCodeByEmail(email);
+        final String savedCode = emailMemoryRepository.findCheckCodeByEmail(email);
 
-
-        if ( (savedCode == null) || (!savedCode.equals(code)) ) {
-            return false;
-        }
-
-
-        // should delete user code
-        return false;
+        return (savedCode != null) && (savedCode.equals(code));
     }
 
 
