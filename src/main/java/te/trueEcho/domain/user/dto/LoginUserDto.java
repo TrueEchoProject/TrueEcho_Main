c346fd23d70aa0cec48c89526bcb83e46d126ee2
HeeJohn <gjshj366@gmail.com>
2024-03-29 13:59:36 +0900

    refactor : 로그인 폼 변경

--- src/main/java/te/trueEcho/domain/user/dto/LoginUserDto.java
+++ src/main/java/te/trueEcho/domain/user/dto/LoginUserDto.java
@@ -1,13 +1,16 @@
 package te.trueEcho.domain.user.dto;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
 @Setter
-@Builder
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class LoginUserDto {
 
-    private String userName;
+    @ApiModelProperty(value = "이메일", example = "trueEcho@gmail.com", required = true)
+    private String email;
+
+    @ApiModelProperty(value = "비밀번호", example = "a12341234", required = true)
     private String password;
 }
