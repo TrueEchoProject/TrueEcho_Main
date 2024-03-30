0d6523834b520fc6d15380f11c562359bf92da2e
HeeJohn <gjshj366@gmail.com>
2024-03-31 03:29:26 +0900

    chore : jwt config

---
+++ src/main/java/te/trueEcho/global/security/jwt/config/SecurityConfig.java
@@ -0,0 +1,96 @@
+package te.trueEcho.global.security.jwt.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import te.trueEcho.domain.user.entity.Role;
+import te.trueEcho.global.security.jwt.JwtAccessDeniedHandler;
+import te.trueEcho.global.security.jwt.JwtAuthenticationEntryPoint;
+import te.trueEcho.global.security.jwt.JwtFilter;
+import te.trueEcho.global.security.jwt.TokenProvider;
+
+import java.io.PrintWriter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // token을 사용하는 방식이기 때문에 csrf를 disable
+
+                .csrf((csrfConfig) ->
+                        csrfConfig.disable()
+                )
+                // 인가 오류 403 jwtAccessDeniedHandler
+                // 인증 오류 401 jwtAuthenticationEntryPoint
+                .exceptionHandling((exceptionHandlingConfigurer) ->
+                        exceptionHandlingConfigurer
+                                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .sessionManagement((exceptionHandlingCustomizer)->
+                        exceptionHandlingCustomizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .requestMatchers("/", "/login/**").permitAll()
+                                .requestMatchers("/posts/**", "/api/v1/posts/**").hasRole(Role.USER.name())
+                                .requestMatchers("/admins/**", "/api/v1/admins/**").hasRole(Role.ADMIN.name())
+                                .anyRequest().authenticated()
+                ).
+                addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+
+    private final AuthenticationEntryPoint unauthorizedEntryPoint =
+            (request, response, authException) -> {
+                ErrorResponse fail = new ErrorResponse(HttpStatus.UNAUTHORIZED, "Spring security unauthorized...");
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                String json = new ObjectMapper().writeValueAsString(fail);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                PrintWriter writer = response.getWriter();
+                writer.write(json);
+                writer.flush();
+            };
+
+    private final AccessDeniedHandler accessDeniedHandler =
+            (request, response, accessDeniedException) -> {
+                ErrorResponse fail = new ErrorResponse(HttpStatus.FORBIDDEN, "Spring security forbidden...");
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                String json = new ObjectMapper().writeValueAsString(fail);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                PrintWriter writer = response.getWriter();
+                writer.write(json);
+                writer.flush();
+            };
+
+    @Getter
+    @RequiredArgsConstructor
+    public class ErrorResponse {
+
+        private final HttpStatus status;
+        private final String message;
+    }
+}
