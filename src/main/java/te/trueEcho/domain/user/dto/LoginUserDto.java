edc7efb61e611587bae51d4f9c8524a151d6c970
HeeJohn <gjshj366@gmail.com>
2024-03-31 03:27:35 +0900

    refactor : validation 추가

--- src/main/java/te/trueEcho/domain/user/dto/LoginUserDto.java
+++ src/main/java/te/trueEcho/domain/user/dto/LoginUserDto.java
@@ -1,5 +1,7 @@
 package te.trueEcho.domain.user.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,8 +11,12 @@ import lombok.*;
 public class LoginUserDto {
 
     @Schema(accessMode = Schema.AccessMode.READ_ONLY, name = "이메일", example = "trueEcho@gmail.com",requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotNull
+    @Size(min = 3, max = 50)
     private String email;
 
     @Schema(accessMode = Schema.AccessMode.READ_ONLY, name = "비밀번호", example = "a12341234",requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotNull
+    @Size(min = 3, max = 100)
     private String password;
 }
