7e95dbe22228d5456eaf92ad1aadb08c4cda72f2
HeeJohn <gjshj366@gmail.com>
2024-03-30 18:56:09 +0900

    refactor : swagger 코드 springdoc lib으로 최신화

--- src/main/java/te/trueEcho/domain/user/entity/Role.java
+++ src/main/java/te/trueEcho/domain/user/entity/Role.java
@@ -1,7 +1,15 @@
 package te.trueEcho.domain.user.entity;
 
-public enum Role {
-    ADMIN,
 
-    USER;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN("ROLE_AMDMIN","일반관리자"),
+    USER("ROLE_USER", "일반사용자");
+
+    private final String key;
+    private final String title;
 }
