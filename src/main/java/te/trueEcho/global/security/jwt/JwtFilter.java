ca8323855ddcf43474f234fa791fd1e415ccac50
HeeJohn <gjshj366@gmail.com>
2024-04-01 22:53:32 +0900

    feat : token 타입 enum 생성

--- src/main/java/te/trueEcho/global/security/jwt/JwtFilter.java
+++ src/main/java/te/trueEcho/global/security/jwt/JwtFilter.java
@@ -7,46 +7,48 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
-import te.trueEcho.global.security.jwt.dto.Token;
+import te.trueEcho.global.security.jwt.dto.TokenType;
 
 import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
-
-    private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
     public static final String AUTHORIZATION_HEADER = "Authorization";
     private final TokenProvider tokenProvider;
 
-    // 실제 필터릴 로직
-    // 토큰의 인증정보를 SecurityContext에 저장하는 역할 수행
+    // 가장 먼저 실행되는 필터
+    // 토큰의 인증정보를 SecurityContext에 저장
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String jwt = resolveToken(httpServletRequest);
-        String requestURI = httpServletRequest.getRequestURI();
-        boolean isRefresh =  httpServletRequest.getRequestURI().contains("/refresh");
-        Token type = isRefresh ? Token.REFRESH : Token.ACCESS;
 
+        String requestURI = httpServletRequest.getRequestURI();
+        TokenType type = isRefreshRequest(requestURI) ? TokenType.REFRESH : TokenType.ACCESS;
+
+        log.warn("=============== this is do Filter ");
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt, type)) {
             UsernamePasswordAuthenticationToken authentication = tokenProvider.getAuthentication(jwt,type);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+            log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
         } else {
-            logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+            log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
+    private boolean isRefreshRequest(String requestUri){
+        return requestUri.contains("/refresh");
+    }
+
     // Request Header 에서 토큰 정보를 꺼내오기 위한 메소드
     private String resolveToken(HttpServletRequest request) {
+        log.warn("=============== this is resolveToken ");
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
 
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
