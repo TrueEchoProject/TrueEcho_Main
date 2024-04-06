381c38877fe9648f8b6682647aae51df625e839b
HolMoly <ryan10920@naver.com>
2024-04-06 18:53:37 +0900

    Refactor: 기본 피드 / 추가 피드 분리

--- navigations/AppTabNavigator/PostTab/FriendPosts.js
+++ navigations/AppTabNavigator/PostTab/FriendPosts.js
@@ -13,33 +13,39 @@ const FriendPosts = React.forwardRef((props, ref) => {
 	const [posts, setPosts] = useState([]); // 게시물 상태 초기화
 	const [refreshing, setRefreshing] = useState(false); // 새로고침 상태를 관리합니다.
 	const pagerViewRef = useRef(null); // PagerView 컴포넌트를 참조하기 위한 ref입니다.
-	const getPosts = async (start = 0) => {
-		
+	const getPosts = async () => {
+		setRefreshing(true);
 		const limit = 10; // 한 번에 불러올 게시물 수
 		try {
-			const url = `http://192.168.0.3:3000/posts?scope=FRIEND&_start=${start}&_limit=${limit}`;
-			const response = await axios.get(url);
+			const response = await axios.get(`http://192.168.0.3:3000/posts?scope=FRIEND&_limit=${limit}`)
+			setPosts(response.data);
+		} catch (error) {
+			console.error('Fetching posts failed:', error);
+		} finally {
+			setRefreshing(false);
+			pagerViewRef.current?.setPageWithoutAnimation(0); // PagerView의 첫 페이지로 이동합니다.
+		}
+	};
+	
+	const getMorePosts = async (start) => {
+		const limit = 10; // 한 번에 불러올 게시물 수
+		try {
+			const response = await axios.get(`http://192.168.0.3:3000/posts?scope=FRIEND&_start=${start}&_limit=${limit}`);
 			const newPosts = response.data;
 			
-			// 새로운 게시물이 없을 경우 로그 출력 및 함수 종료
 			if (newPosts.length === 0) {
 				console.log("No more posts to load.");
 				return;
 			}
 			
-			// start가 0이면 게시물 목록을 초기화, 그렇지 않으면 기존 목록에 추가
-			setPosts(start === 0 ? newPosts : prevPosts => [...prevPosts, ...newPosts]);
+			setPosts(prevPosts => [...prevPosts, ...newPosts]); // 기존 목록에 추가
 		} catch (error) {
-			console.error('Fetching posts failed:', error);
-		} finally {
-		
+			console.error('Fetching more posts failed:', error);
 		}
 	};
 	
 	const refreshPosts = async () => {
-		setRefreshing(true);
 		await getPosts(); // 데이터를 새로고침합니다.
-		setRefreshing(false);
 		pagerViewRef.current?.setPageWithoutAnimation(0); // PagerView의 첫 페이지로 이동합니다.
 	};
 	
@@ -77,7 +83,7 @@ const FriendPosts = React.forwardRef((props, ref) => {
 					onPageSelected={e => {
 						const newIndex = e.nativeEvent.position;
 						if (newIndex === posts.length - 4) {
-							getPosts(posts.length);
+							getMorePosts(posts.length);
 						}
 					}}
 				>
