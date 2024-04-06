a11b2ef25d3bd255ede2b6dba9ed71ec13be126a
HeeJohn <gjshj366@gmail.com>
2024-04-07 04:29:18 +0900

    feat : 같은 지역에 사는 친구들 찾기

--- src/main/java/te/trueEcho/domain/user/repository/UserRepository.java
+++ src/main/java/te/trueEcho/domain/user/repository/UserRepository.java
@@ -3,19 +3,39 @@ package te.trueEcho.domain.user.repository;
 
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import te.trueEcho.domain.user.entity.User;
 
 import java.util.List;
 
 @Repository
+@Slf4j
 @RequiredArgsConstructor
 public class UserRepository {
     private final EntityManager em;
+
+
+    @Transactional
     public List<User> filterSelectedUserByLocation(String location, List<User> selectedUsers ){
-      return  em.createQuery("select u from User u where u.id not in : selectedUsers and u.location =:location", User.class)
+      return  em.createQuery("select u from User u where u  in : selectedUsers and u.location like :location", User.class)
                 .setParameter("selectedUsers",selectedUsers)
                 .setParameter("location",location)
                 .getResultList();
+    }
+
+    @Transactional
+    public List<User> filterNotSelectedUserByLocation(String location, List<User> selectedUsers ){
+        try {
+            return em.createQuery("select u from User u where (u not in : selectedUsers) " +
+                            "and u.location like :location", User.class)
+                    .setParameter("selectedUsers", selectedUsers)
+                    .setParameter("location", location)
+                    .getResultList();
+        }catch (Exception e){
+            log.info("query exception = {}", e);
+            return null;
+        }
     }
 }
