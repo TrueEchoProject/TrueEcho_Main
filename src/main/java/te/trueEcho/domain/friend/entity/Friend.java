da313c7d7b4ce759c81aae2f31e3788893c19296
jun10920 <abide0206@naver.com>
2024-04-12 22:54:47 +0900

    feat: 친구 관련 기능 추가

--- src/main/java/te/trueEcho/domain/friend/entity/Friend.java
+++ src/main/java/te/trueEcho/domain/friend/entity/Friend.java
@@ -10,12 +10,12 @@ import te.trueEcho.domain.user.entity.User;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "friends",   uniqueConstraints={
-        @UniqueConstraint(
-                name= "one-way-friendship",
-                columnNames={"target_user_id", "user_id"}
-        )
-})
+//@Table(name = "friends",   uniqueConstraints={
+//        @UniqueConstraint(
+//                name= "one-way-friendship",
+//                columnNames={"target_user_id", "user_id"}
+//        )
+//})
 
 public class Friend {
 
