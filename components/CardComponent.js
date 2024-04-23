f48f9943159aea5405049def2ab7a88ece36e7c9
HolMoly <ryan10920@naver.com>
2024-04-23 18:53:20 +0900

    Feat: 사용자 차단 시, 대상 추출 및 전달

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Share, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Share, Dimensions} from 'react-native';
 import axios from 'axios';
 import { Image } from 'expo-image';
-import { MaterialIcons, Ionicons, Feather, SimpleLineIcons } from "@expo/vector-icons";
+import { MaterialIcons, Ionicons, Feather } from "@expo/vector-icons";
 import { ImageButton } from "./ImageButton";
 import { CommentModal } from './CommentModal'; // 댓글 창 컴포넌트 임포트
 
-const CardComponent = ({ post, isOptionsVisibleExternal, setIsOptionsVisibleExternal }) => {
-	const [isOptionsVisible, setIsOptionsVisible] = useState(isOptionsVisibleExternal || false);
+const CardComponent = ({ post }) => {
+	const [isOptionsVisible, setIsOptionsVisible] = useState(false);
 	const [buttonLayout, setButtonLayout] = useState({ x: 0, y: 0, width: 0, height: 0 });
 	const [imageButtonHeight, setImageButtonHeight] = useState(0);
 	const [isLiked, setIsLiked] = useState(false); // 좋아요 상태 관리
@@ -15,17 +15,8 @@ const CardComponent = ({ post, isOptionsVisibleExternal, setIsOptionsVisibleExte
 	const [isCommentVisible, setIsCommentVisible] = useState(false); // 댓글 창 표시 상태
 	const [layoutSet, setLayoutSet] = useState(false); // 레이아웃 설정 여부 상태 추가
 	const windowWidth = Dimensions.get('window').width;
-	const [friendLook, setFriendLook] = useState(true); // 좋아요 수 관리
-	
-	useEffect(() => {
-		setIsOptionsVisible(isOptionsVisibleExternal);
-		console.log(`Options Visible for ${post.post_id}: ${isOptionsVisibleExternal}`);
-	}, [isOptionsVisibleExternal]); // 이제 외부에서 받은 props가 변경될 때마다 로그를 찍고 상태를 업데이트합니다.
-	
 	const toggleOptionsVisibility = () => {
-		const newVisibility = !isOptionsVisible;
-		setIsOptionsVisible(newVisibility); // 내부 상태 업데이트
-		setIsOptionsVisibleExternal(newVisibility); // 외부 상태 업데이트로 전파
+		setIsOptionsVisible(!isOptionsVisible);
 	};
 	const onImageButtonLayout = (event) => {
 		if (layoutSet) return; // 레이아웃이 이미 설정되었다면 추가 업데이트 방지
@@ -37,7 +28,6 @@ const CardComponent = ({ post, isOptionsVisibleExternal, setIsOptionsVisibleExte
 	const hideOptions = () => {
 		if (isOptionsVisible) {
 			setIsOptionsVisible(false);
-			setIsOptionsVisibleExternal(false); // 외부 상태도 업데이트
 		}
 	};
 	const toggleLike = async () => {
@@ -62,21 +52,8 @@ const CardComponent = ({ post, isOptionsVisibleExternal, setIsOptionsVisibleExte
 				blocked_user: post.username  // 이제 post.username을 직접 사용합니다.
 			});
 			console.log('blocked_users updated successfully');
-			hideOptions()
 		} catch (error) {
 			console.error('Error updating blocked_users:', error);
-		}
-	};
-	const toggleFriendSend = async () => {
-		console.log(post.username);  // 이것은 여전히 정상적으로 작동합니다.
-		try {
-			await axios.post(`http://192.168.0.3:3000/friendSend`, {
-				friendSendUser: post.username
-			});
-			console.log('Send updated successfully');
-			setFriendLook(false);
-		} catch (error) {
-			console.error('Error updating Send:', error);
 		}
 	};
 	const toggleCommentVisibility = () => {
@@ -98,62 +75,20 @@ const CardComponent = ({ post, isOptionsVisibleExternal, setIsOptionsVisibleExte
 							<Text style={{fontSize: 12, fontWeight: "300"}}>{post.location}</Text>
 						</View>
 					</View>
-					<View style={{
-						flexDirection: "column",
-						marginLeft: "auto",
+					<TouchableOpacity style={styles.right} onPress={toggleOptionsVisibility} onLayout={(event) => {
+						const layout = event.nativeEvent.layout;
+						setButtonLayout(layout);
 					}}>
-						{ post.friend === 1 && ( friendLook === true ? (
-								<View style={[
-									styles.right,
-									{
-										backgroundColor: "#3B4664",
-										padding: 5,
-										marginBottom: 5,
-										borderRadius: 3,
-									}
-								]}>
-									<TouchableOpacity onPress={toggleFriendSend}>
-										<Text style={{
-											fontSize: 15,
-											color: "white",
-										}}>
-											친구 추가
-										</Text>
-									</TouchableOpacity>
-								</View>
-							) : (
-								<View style={[
-									styles.right,
-									{
-										backgroundColor: "#3B4664",
-										padding: 5,
-										marginBottom: 5,
-										borderRadius: 3,
-									}
-								]}>
-									<Text style={{
-										fontSize: 15,
-										color: "white",
-									}}>
-										추가 완료
-									</Text>
-								</View>
-							)
-						)}
-						<TouchableOpacity style={styles.right} onPress={toggleOptionsVisibility} onLayout={(event) => {
-							const layout = event.nativeEvent.layout;
-							setButtonLayout(layout);
-						}}>
-							<SimpleLineIcons name="options-vertical" size={20} color="black" />
-						</TouchableOpacity>
-					</View>
+						<Text style={{fontSize: 30}}>...</Text>
+					</TouchableOpacity>
 				</View>
 				{isOptionsVisible && (
 					<View style={[
 						styles.optionsContainer,
-						post.friend === 1 ?
-							{ top: buttonLayout.y + buttonLayout.height, right: 0 } :
-							{ top: buttonLayout.y + buttonLayout.height + 30, right: 0 }
+						{
+							top: buttonLayout.y + buttonLayout.height - 5,
+							right: 0
+						}
 					]}>
 						<TouchableOpacity onPress={toggleBlock} style={{ flexDirection:'row', alignItems: 'center', }}>
 							<Feather name='alert-triangle' style={{ marginLeft: 10, color: 'red' }}/>
@@ -191,25 +126,6 @@ const CardComponent = ({ post, isOptionsVisibleExternal, setIsOptionsVisibleExte
 							postId={post.post_id}
 							onClose={() => setIsCommentVisible(false)}
 						/>
-						{post.post_status === 1 || post.post_status === 2 ? (
-							<View style={[
-								styles.right,
-								{
-									marginLeft: 'auto',
-									padding: 5,
-									paddingLeft: 30,
-									paddingRight: 30,
-									backgroundColor: "#3B4664",
-									borderRadius: 10,}
-							]}>
-								{post.post_status === 1 && (
-									<Text style={{ color: "white", fontSize: 25}}>free</Text>
-								)}
-								{post.post_status === 2 && (
-									<Text style={{ color: "white", fontSize: 25}}>late</Text>
-								)}
-							</View>
-						) : null}
 					</View>
 				</View>
 			</View>
