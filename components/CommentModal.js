754f88aef043c7522a85462fc9b52700d6ea605b
HolMoly <ryan10920@naver.com>
2024-04-23 18:46:46 +0900

    Feat: 슬라이드에 의한 댓글 창 애니메이션

--- components/CommentModal.js
+++ components/CommentModal.js
@@ -11,17 +11,62 @@ import {
 	PanResponder,
 	Animated,
 	Platform,
+	Keyboard,
 	KeyboardAvoidingView,
 } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import axios from 'axios';
 import { Image } from 'expo-image';
 
 const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
 export const CommentModal = React.memo(({ isVisible, postId, onClose }) => {
+	const insets = useSafeAreaInsets(); // Safe Area 여백 얻기
+	const initialMarginTop = Platform.OS === 'ios' ? windowHeight * 0.3 + insets.top : windowHeight * 0.3;
+	const marginTopLimit = Platform.OS === 'ios' ? insets.top : 0;
 	const [comments, setComments] = useState([]);
-	const [loading, setLoading] = useState(false); // 로딩 상태 추가
+	const [loading, setLoading] = useState(false);
 	const [showUnderComments, setShowUnderComments] = useState({});
 	const [textInputValue, setTextInputValue] = useState('');
+	const animatedHeight = useRef(new Animated.Value(initialMarginTop)).current;
+	
+	const handleInputFocus = () => {
+		// 키보드가 올라올 때 모달을 상단으로 이동시키는 애니메이션
+		Animated.timing(animatedHeight, {
+			toValue: marginTopLimit, // 모달을 최상단까지 올림 (iOS의 경우 Safe Area 고려)
+			duration: 300, // 키보드 올라오는 시간과 유사하게 설정
+			useNativeDriver: false,
+		}).start();
+	};
+	
+	const panResponder = useMemo(() => PanResponder.create({
+		onStartShouldSetPanResponder: () => true,
+		onPanResponderMove: (_, gestureState) => {
+			const dragResponse = gestureState.dy * 0.1;
+			let newMarginTop = animatedHeight._value + dragResponse;
+			
+			
+			newMarginTop = Math.min(Math.max(newMarginTop, marginTopLimit), windowHeight);
+			animatedHeight.setValue(newMarginTop);
+		},
+		onPanResponderRelease: (_, gestureState) => {
+			// 아래로 충분한 거리 슬라이드 시 모달 닫기
+			if (animatedHeight._value >= windowHeight * 0.5) { // 150은 임계값, 조정 가능
+				onClose(); // 모달 닫기 함수 호출
+			} else if (animatedHeight._value < windowHeight * 0.15) {
+				Animated.spring(animatedHeight, {
+					toValue: marginTopLimit,
+					useNativeDriver: false
+				}).start();
+			} else {
+				// 슬라이드를 멈췄을 때, 높이가 애매한 위치에 있을 경우 원래 높이로 복귀
+				Animated.spring(animatedHeight, {
+					toValue: initialMarginTop,
+					useNativeDriver: false
+				}).start();
+			}
+		},
+	}), [insets.top]); // 의존성 배열 업데이트
 	
 	useEffect(() => {
 		if (isVisible) {
@@ -37,6 +82,7 @@ export const CommentModal = React.memo(({ isVisible, postId, onClose }) => {
 				});
 		} else {
 			setComments([]); // 모달이 닫힐 때 댓글 상태 초기화
+			animatedHeight.setValue(windowHeight * 0.3); // 모달 높이 초기화
 		}
 	}, [isVisible, postId]);
 	
@@ -67,6 +113,7 @@ export const CommentModal = React.memo(({ isVisible, postId, onClose }) => {
 			</View>
 		) : null;
 	});
+	
 	const toggleUnderComments = (index) => {
 		setShowUnderComments(prevState => ({
 			...prevState,
@@ -75,59 +122,63 @@ export const CommentModal = React.memo(({ isVisible, postId, onClose }) => {
 	};
 	
 	return (
-		<Modal
-			animationType="slide"
-			visible={isVisible}
-			onRequestClose={onClose}
-			transparent={true}
-		>
-			<View style={styles.modalOverlay}>
-				<KeyboardAvoidingView
-					behavior={Platform.OS === "ios" ? "padding" : undefined}
-					style={styles.modalView}
-				>
-					<TouchableOpacity style={styles.closeButton} onPress={onClose}>
-						<Text>닫기</Text>
-					</TouchableOpacity>
-					<ScrollView
-						style={styles.commentsList}
-						contentContainerStyle={{ paddingBottom: 20 }} // ScrollView의 paddingBottom 조정
-						keyboardShouldPersistTaps="handled"
+		<SafeAreaProvider>
+			<Modal
+				animationType="slide"
+				visible={isVisible}
+				onRequestClose={onClose}
+				transparent={true}
+			>
+				<View style={styles.modalOverlay}>
+					<KeyboardAvoidingView
+						behavior={Platform.OS === "ios" ? "padding" : undefined}
+						{...panResponder.panHandlers}
 					>
-						{loading ? (
-							<Text>Loading comments...</Text> // 로딩 인디케이터 표시
-						) : (
-							<>
-								{comments.map((comment, index) => (
-									<CommentItem
-										key={index}
-										comment={comment}
-										toggleUnderComments={toggleUnderComments}
-										showUnderComments={showUnderComments}
-										index={index}
-									/>
-								))}
-							</>
-						)}
-					</ScrollView>
-					<View style={styles.inputContainer}>
-						<TextInput
-							style={styles.input}
-							value={textInputValue}
-							onChangeText={setTextInputValue}
-							placeholder="댓글 추가..."
-						/>
-					</View>
-				</KeyboardAvoidingView>
-			</View>
-		</Modal>
+						<Animated.View style={[styles.modalView, { marginTop: animatedHeight }]}>
+							<TouchableOpacity style={styles.closeButton} onPress={onClose}>
+								<Text>닫기</Text>
+							</TouchableOpacity>
+							<ScrollView
+								style={styles.commentsList}
+								contentContainerStyle={{ paddingBottom: 20 }} // ScrollView의 paddingBottom 조정
+								keyboardShouldPersistTaps="handled"
+							>
+								{loading ? (
+									<Text>Loading comments...</Text> // 로딩 인디케이터 표시
+								) : (
+									<>
+										{comments.map((comment, index) => (
+											<CommentItem
+												key={index}
+												comment={comment}
+												toggleUnderComments={toggleUnderComments}
+												showUnderComments={showUnderComments}
+												index={index}
+											/>
+										))}
+									</>
+								)}
+							</ScrollView>
+							<View style={styles.inputContainer}>
+								<TextInput
+									style={styles.input}
+									value={textInputValue}
+									onChangeText={setTextInputValue}
+									placeholder="댓글 추가..."
+									onFocus={handleInputFocus}
+								/>
+							</View>
+						</Animated.View>
+					</KeyboardAvoidingView>
+				</View>
+			</Modal>
+		</SafeAreaProvider>
 	);
 })
 
 const styles = StyleSheet.create({
 	modalOverlay: {
 		flex: 1,
-		justifyContent: 'flex-end',
 		alignItems: 'center',
 		backgroundColor: 'rgba(0, 0, 0, 0.5)', // 반투명 배경
 	},
@@ -149,9 +200,9 @@ const styles = StyleSheet.create({
 	modalView: {
 		borderTopRightRadius: 20,
 		borderTopLeftRadius: 20,
-		width:"100%",
+		width: windowWidth,
 		flex: 1,
-		marginTop: windowHeight * 0.2,
+		marginTop: windowHeight * 0.3,
 		backgroundColor: 'white',
 		shadowColor: '#000',
 		shadowOffset: {
