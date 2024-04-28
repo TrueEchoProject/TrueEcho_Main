12e3f2f425423fe1423e0aa2379715515d35babb
HolMoly <ryan10920@naver.com>
2024-04-28 18:16:39 +0900

    Refactor: 오류 수정

--- navigations/AppTabNavigator/CommunityTabs/CommunityTab.jsx
+++ navigations/AppTabNavigator/CommunityTabs/CommunityTab.jsx
@@ -8,7 +8,8 @@ export const CommunityTabScreen = () => {
 	return (
 		<CommunityTab.Navigator
 			screenOptions={{
-				tabBarStyle: { display: 'none' } // 하단 탭 바 숨김
+				tabBarStyle: { display: 'none' }, // 하단 탭 바 숨김
+				headerShown: false
 			}}
 			initialRouteName="투표" // 최초 어플 실행 시 FriendFeed가 기본 화면으로 설정
 		>
