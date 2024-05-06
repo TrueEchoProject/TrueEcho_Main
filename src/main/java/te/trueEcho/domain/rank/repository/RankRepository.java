e9f19d84e54a6a5bfe09e3942e1f5e434c10131c
HeeJohn <gjshj366@gmail.com>
2024-05-06 16:42:33 +0900

    refactor : 캐시에서 바로 반환

--- src/main/java/te/trueEcho/domain/rank/repository/RankRepository.java
+++ src/main/java/te/trueEcho/domain/rank/repository/RankRepository.java
@@ -1,18 +1,12 @@
 package te.trueEcho.domain.rank.repository;
 
 
-import te.trueEcho.domain.user.entity.User;
-import te.trueEcho.domain.vote.entity.Vote;
-import te.trueEcho.domain.vote.entity.VoteResult;
-
-import java.time.LocalDate;
-import java.time.temporal.ChronoField;
-import java.util.List;
-import java.util.Map;
+import te.trueEcho.domain.rank.dto.RankListResponse;
 
 public interface RankRepository {
 
-     Map<Vote,Map<User, Integer>>  getRanksByWeek();
+     RankListResponse getRanksByWeek();
 
-     void cacheThisWeekRank( Map<Vote,Map<User, Integer>>  voteResultMap);
+     void cacheThisWeekRank( RankListResponse voteResultMap);
+
 }
