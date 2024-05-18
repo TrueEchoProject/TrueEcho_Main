17d117ef5b677fa5b09f68e125d72d4ceef09cba
jun10920 <abide0206@naver.com>
2024-05-18 16:21:21 +0900

    chore: merge

--- src/main/java/te/trueEcho/global/response/ResponseCode.java
+++ src/main/java/te/trueEcho/global/response/ResponseCode.java
@@ -69,25 +69,58 @@ public enum ResponseCode {
     DELETE_BLCOK_FAIL(202, "T002", "차단 해제에 실패했습니다."),
 
     // 게시물
-    GET_POST_SUCCESS(202, "T002", "게시물을 읽기를 성공했습니다."),
-    GET_POST_FAIL(202, "T002", "게시물을 읽기를 실패했습니다."),
-    GET_COMMENT_SUCCESS(202, "T002", "해당 게시물의 댓글 읽기를 성공했습니다."),
-    GET_COMMENT_FAIL(202, "T002", "해당 게시물의 댓글 읽기를 실패했습니다."),
+    GET_POST_SUCCESS(202, "T002", "게시물을 조회를 성공했습니다."),
+    GET_POST_FAIL(202, "T002", "게시물을 조회를 실패했습니다."),
+    GET_COMMENT_SUCCESS(202, "T002", "해당 게시물의 댓글 조회를 성공했습니다."),
+    GET_COMMENT_FAIL(202, "T002", "해당 게시물의 댓글 조회를 실패했습니다."),
     WRITE_POST_SUCCESS(202, "T002", "게시물 작성을 성공했습니다."),
     WRITE_POST_FAIL(202, "T002", "게시물 작성을 실패했습니다."),
 
 
     // 투표
-    GET_VOTE_CONTENT_SUCCESS(202, "T002", "투표지 읽기를 성공했습니다."),
-    GET_VOTE_CONTENT_FAIL(202, "T002", "투표지 읽기를 실패했습니다."),
+    GET_VOTE_CONTENT_SUCCESS(202, "T002", "투표지 조회를 성공했습니다."),
+    GET_VOTE_CONTENT_FAIL(202, "T002", "투표지 조회를 실패했습니다."),
     GET_VOTE_TARGET_SUCCESS(202, "T002", "투표 인원 뽑기를 성공했습니다."),
     GET_VOTE_TARGET_FAIL(202, "T002","투표 인원 뽑기를 실패했습니다."),
     SAVE_VOTE_RESULT_SUCCESS(202, "T002", "투표 결과 저장을 성공했습니다."),
     SAVE_VOTE_RESULT_FAIL(202, "T002","투표 결과 저장을 실패했습니다."),
 
     // 랭킹
-    GET_RANK_SUCCESS(202, "T002", "투표지 읽기를 성공했습니다."),
-    GET_RANK_FAIL(202, "T002", "투표지 읽기를 실패했습니다."),
+    GET_RANK_SUCCESS(202, "T002", "랭킹 조회를 성공했습니다."),
+    GET_RANK_FAIL(202, "T002", "랭킹 조회를 실패했습니다."),
+
+    //// 설정
+    // 마이페이지
+    GET_MY_PAGE_SUCCESS(202, "T002", "마이페이지 조회를 성공했습니다."),
+    GET_MY_PAGE_FAIL(202, "T002", "마이페이지 조회를 실패했습니다."),
+    // 핀
+    GET_PINS_SUCCESS(202, "T002", "핀 조회를 성공했습니다."),
+    GET_PINS_FAIL(202, "T002", "핀 조회를 실패했습니다."),
+    PUT_PINS_SUCCESS(202, "T002", "핀 수정을 성공했습니다."),
+    PUT_PINS_FAIL(202, "T002", "핀 수정을 실패했습니다."),
+    // 캘린더 - 월별 게시물
+    GET_MONTHLY_POST_SUCCESS(202, "T002", "월별 게시물 조회를 성공했습니다."),
+    GET_MONTHLY_POST_FAIL(202, "T002", "월별 게시물 조회를 실패했습니다."),
+    // 개인정보
+    GET_MY_INFO_SUCCESS(202, "T002", "개인정보 조회를 성공했습니다."),
+    GET_MY_INFO_FAIL(202, "T002", "개인정보 조회를 실패했습니다."),
+    PUT_MY_INFO_SUCCESS(202, "T002", "개인정보 수정를 성공했습니다."),
+    PUT_MY_INFO_FAIL(202, "T002", "개인정보 수정를 실패했습니다."),
+    // 차단
+    GET_BLOCKED_USER_SUCCESS(202, "T002", "차단한 유저 조회를 성공했습니다."),
+    GET_BLOCKED_USER_FAIL(202, "T002", "차단한 유저 조회를 실패했습니다."),
+    PUT_BLOCKED_USER_SUCCESS(202, "T002", "차단한 유저 수정를 성공했습니다."),
+    PUT_BLOCKED_USER_FAIL(202, "T002", "차단한 유저 수정를 실패했습니다."),
+    // 포토타임
+    GET_NOTIFY_TIME_SUCCESS(202, "T002", "랜덤알림 시간 조회를 성공했습니다."),
+    GET_NOTIFY_TIME_FAIL(202, "T002", "랜덤알림 시간 조회를 실패했습니다."),
+    PUT_NOTIFY_TIME_SUCCESS(202, "T002", "랜덤알림 시간 수정를 성공했습니다."),
+    PUT_NOTIFY_TIME_FAIL(202, "T002", "랜덤알림 시간 수정를 실패했습니다."),
+    // 알림창
+    GET_NOTIFICATION_OPTION_SUCCESS(202, "T002", "알림 옵션 조회를 성공했습니다."),
+    GET_NOTIFICATION_OPTION_FAIL(202, "T002", "알림 옵션 조회를 실패했습니다."),
+    PUT_NOTIFICATION_OPTION_SUCCESS(202, "T002", "알림 옵션 수정를 성공했습니다."),
+    PUT_NOTIFICATION_OPTION_FAIL(202, "T002", "알림 옵션 수정를 실패했습니다."),
 
     // 알림
     FCM_TOKEN_SAVED_SUCCESS(202, "T002", "FCM 토큰 저장에 성공했습니다."),
