e5c62a1c17a497cde24888f54a9100b85460de85
HeeJohn <gjshj366@gmail.com>
2024-05-20 00:04:29 +0900

    chore : 오타 수정 respons->responses

--- src/main/java/te/trueEcho/domain/post/dto/PostListResponse.java
+++ src/main/java/te/trueEcho/domain/post/dto/PostListResponse.java
@@ -11,5 +11,5 @@ import java.util.List;
 public class PostListResponse {
     String yourLocation;
     int postCount;
-    List<ReadPostResponse> readPostRespons;
+    List<ReadPostResponse> readPostResponse;
 }
