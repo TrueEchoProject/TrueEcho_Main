0959906f562c5524785e65b8b4156b788b63819a
백효영 <hyoyoung@baeghyoyeong-ui-noteubug.local>
2024-07-01 13:20:55 +0900

    스택 네비 디자인 적용

--- navigations/Stack.js
+++ navigations/Stack.js
@@ -1,92 +1,153 @@
 import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
-import { Friends, MyPage, MyOptions, Calendar, MyInfo, Alarm, FeedAlarm, UserAlarm, IsAlarm, MyFeed } from "./AppTabNavigator/ButtonStack"
-import { View } from 'react-native';
-import { Button1, Button2 } from "../components/Button";
+import { View, Text, Image } from 'react-native';
+import { MaterialIcons, AntDesign } from "@expo/vector-icons";
 import { MainPostTabScreen } from "./AppTabNavigator/PostTab/MainPostTab";
 import { CommunityTabScreen } from "./AppTabNavigator/CommunityTabs/CommunityTab";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+import {
+  Friends as FriendsComponent,
+  MyPage as MyPageComponent,
+  MyOptions as MyOptionsComponent,
+  Calendar as CalendarComponent,
+  MyInfo as MyInfoComponent,
+  Alarm as AlarmComponent,
+  FeedAlarm as FeedAlarmComponent,
+  UserAlarm as UserAlarmComponent,
+  IsAlarm as IsAlarmComponent,
+  MyFeed as MyFeedComponent
+} from "./AppTabNavigator/ButtonStack";
 
 const MainPostStack = createStackNavigator();
-
-export const MainPostStackScreen = () => { // 메인 피드 속 Stack 구성
-	return (
-		<MainPostStack.Navigator>
-			<MainPostStack.Screen
-				name="FeedTab"
-				component={MainPostTabScreen}
-				options={({ navigation }) => ({ // 메인 피드 화면에서 상단 네비 구현
-					headerRight: () => (
-						<View style={{flexDirection: 'row'}}>
-							<Button1 onPress={() => navigation.navigate('Fri')} />
-							<Button2 onPress={() => navigation.navigate('MyP')} />
-						</View>
-					),
-					headerLeft: null
-				})}
-			/>
-			<MainPostStack.Screen
-				name="MyP"
-				component={MyPage}
-			/>
-			<MainPostStack.Screen
-				name="Fri"
-				component={Friends}
-			/>
-			<MainPostStack.Screen
-				name="MyOp"
-				component={MyOptions}
-			/>
-			<MainPostStack.Screen
-				name="Calendar"
-				component={Calendar}
-			/>
-			<MainPostStack.Screen
-				name="MyInfo"
-				component={MyInfo}
-			/>
-			<MainPostStack.Screen
-				name="Alarm"
-				component={Alarm}
-			/>
-			<MainPostStack.Screen
-				name="FeedAlarm"
-				component={FeedAlarm}
-			/>
-			<MainPostStack.Screen
-				name="UserAlarm"
-				component={UserAlarm}
-			/>
-			<MainPostStack.Screen
-				name="IsAlarm"
-				component={IsAlarm}
-			/>
-			<MainPostStack.Screen
-				name="MyFeed"
-				component={MyFeed}
-			/>
-		</MainPostStack.Navigator>
-	);
-}
-
 const CommunityStack = createStackNavigator();
 
-export const CommunityStackScreen = () => { // 커뮤니티 속 Stack 구성
-	return (
-		<CommunityStack.Navigator>
-			<CommunityStack.Screen
-				name="Community"
-				component={CommunityTabScreen}
-				options={({ navigation }) => ({ // 커뮤니티 화면에서 상단 네비 구현
-					headerShown: false,
-					headerRight: () => (
-						<View style={{flexDirection: 'row'}}>
-							<Button1 onPress={() => navigation.navigate('Fri')} />
-							<Button2 onPress={() => navigation.navigate('MyP')} />
-						</View>
-					),
-					headerLeft: null
-				})}
-			/>
-		</CommunityStack.Navigator>
-	);
-}
+const CustomHeaderLeft = ({ navigation, title }) => (
+  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+    <AntDesign
+      name="left"
+      size={32}
+      color="white"
+      onPress={() => navigation.goBack()}
+      style={{ marginHorizontal: 10 }}
+    />
+    <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>{title}</Text>
+  </View>
+);
+
+const LogoImage = () => (
+  <Image source={require('../assets/logoFont.png')} style={{ width: wp('35%'), height: hp('5%'), marginLeft: 10 }} />
+);
+
+const getHeaderLeft = (navigation, route) => {
+  const title = route.name === 'FeedTab' ? '피드 탭' :
+    route.name === 'MyP' ? '마이 페이지' :
+    route.name === 'Fri' ? '친구' :
+    route.name === 'MyOp' ? '내 옵션' :
+    route.name === 'Calendar' ? '캘린더' :
+    route.name === 'MyInfo' ? '내 정보 수정' :
+    route.name === 'Alarm' ? '알림' :
+    route.name === 'FeedAlarm' ? '피드 알림' :
+    route.name === 'UserAlarm' ? '유저 알림' :
+    route.name === 'IsAlarm' ? '아이즈 알림' :
+    route.name === 'MyFeed' ? '내 피드' : route.name;
+
+  return route.name === 'FeedTab'
+    ? <LogoImage />
+    : <CustomHeaderLeft navigation={navigation} title={title} />;
+};
+
+const MainPostStackScreen = () => {
+  return (
+    <MainPostStack.Navigator
+      screenOptions={({ navigation, route }) => ({
+        headerTitle: '',
+        headerStyle: {
+          backgroundColor: '#000000',
+        },
+        headerTintColor: '#ffffff',
+        headerLeft: () => getHeaderLeft(navigation, route),
+        headerRight: () => (
+          <View style={{ flexDirection: 'row' }}>
+            <MaterialIcons
+              name="emoji-people"
+              size={24}
+              color="white"
+              style={{ marginHorizontal: 10 }}
+              onPress={() => navigation.navigate('Fri', { title: '친구' })}
+            />
+            <MaterialIcons
+              name="assignment-ind"
+              size={24}
+              color="white"
+              style={{ marginHorizontal: 10 }}
+              onPress={() => navigation.navigate('MyP', { title: '마이 페이지' })}
+            />
+          </View>
+        ),
+      })}
+    >
+      <MainPostStack.Screen
+        name="FeedTab"
+        component={MainPostTabScreen}
+        options={{ title: '피드 탭' }}
+      />
+      <MainPostStack.Screen name="MyP" component={MyPageComponent} options={{ title: '마이 페이지' }} />
+      <MainPostStack.Screen name="Fri" component={FriendsComponent} options={{ title: '친구' }} />
+      <MainPostStack.Screen name="MyOp" component={MyOptionsComponent} options={{ title: '내 옵션' }} />
+      <MainPostStack.Screen name="Calendar" component={CalendarComponent} options={{ title: '캘린더' }} />
+      <MainPostStack.Screen name="MyInfo" component={MyInfoComponent} options={{ title: '내 정보 수정' }} />
+      <MainPostStack.Screen name="Alarm" component={AlarmComponent} options={{ title: '알림' }} />
+      <MainPostStack.Screen name="FeedAlarm" component={FeedAlarmComponent} options={{ title: '피드 알림' }} />
+      <MainPostStack.Screen name="UserAlarm" component={UserAlarmComponent} options={{ title: '유저 알림' }} />
+      <MainPostStack.Screen name="IsAlarm" component={IsAlarmComponent} options={{ title: '아이즈 알림' }} />
+      <MainPostStack.Screen name="MyFeed" component={MyFeedComponent} options={{ title: '내 피드' }} />
+    </MainPostStack.Navigator>
+  );
+};
+
+const CommunityStackScreen = () => {
+  return (
+    <CommunityStack.Navigator
+      screenOptions={({ navigation, route }) => ({
+        headerTitle: '',
+        headerStyle: {
+          backgroundColor: '#000000',
+        },
+        headerTintColor: '#ffffff',
+        headerLeft: () => {
+          const title = route.name === 'Community' ? '커뮤니티' : route.name;
+          return route.name === 'Community'
+            ? <LogoImage />
+            : <CustomHeaderLeft navigation={navigation} title={title} />;
+        },
+        headerRight: () => (
+          <View style={{ flexDirection: 'row' }}>
+            <MaterialIcons
+              name="emoji-people"
+              size={24}
+              color="white"
+              style={{ marginHorizontal: 10 }}
+              onPress={() => navigation.navigate('Fri', { title: '친구' })}
+            />
+            <MaterialIcons
+              name="assignment-ind"
+              size={24}
+              color="white"
+              style={{ marginHorizontal: 10 }}
+              onPress={() => navigation.navigate('MyP', { title: '마이 페이지' })}
+            />
+          </View>
+        ),
+      })}
+    >
+      <CommunityStack.Screen
+        name="Community"
+        component={CommunityTabScreen}
+        options={{ title: '커뮤니티' }}
+      />
+    </CommunityStack.Navigator>
+  );
+};
+
+export { MainPostStackScreen, CommunityStackScreen };
