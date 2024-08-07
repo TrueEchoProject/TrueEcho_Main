8c7e5a742bd0ff70116fb48eb9050e50c0dcb08e
HyoYoung0829 <108615164+HyoYoung0829@users.noreply.github.com>
2024-08-08 00:23:40 +0900

    비밀번호 가리기 버튼 추가

--- SignUp/LoginForm.jsx
+++ SignUp/LoginForm.jsx
@@ -22,6 +22,7 @@ import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
 import { LinearGradient } from 'expo-linear-gradient';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -32,6 +33,7 @@ const LoginForm = () => {
   const [expoPushToken, setExpoPushToken] = useState('');
   const [warning, setWarning] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false); // 비밀번호 토글
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
@@ -188,7 +190,7 @@ const LoginForm = () => {
   }
 
   return (
-    <SafeAreaView  style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="black" />
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require('../assets/logo.png')} />
@@ -209,14 +211,19 @@ const LoginForm = () => {
           </View>
           <View style={styles.inputComposition}>
             <Text style={styles.inputText}>비밀번호</Text>
-            <TextInput
-              placeholder="비밀번호를 입력해주세요."
-              value={loginData.password}
-              onChangeText={(text) => handleChange("password", text)}
-              style={styles.input}
-              secureTextEntry
-              placeholderTextColor="#ccc"
-            />
+            <View style={styles.passwordContainer}>
+              <TextInput
+                placeholder="비밀번호를 입력해주세요."
+                value={loginData.password}
+                onChangeText={(text) => handleChange("password", text)}
+                style={styles.passwordInput}
+                secureTextEntry={!showPassword}
+                placeholderTextColor="#ccc"
+              />
+              <Pressable onPress={() => setShowPassword(!showPassword)} style={styles.toggleButton}>
+                <Ionicons name={showPassword ? "eye-off-outline" : "eye-outline"} size={24} color="white" />
+              </Pressable>
+            </View>
             {warning === "passwordEmpty" && <Text style={styles.warningText}>비밀번호를 입력해주세요.</Text>}
             {warning === "shortPassword" && <Text style={styles.warningText}>비밀번호는 최소 6자 이상이어야 합니다.</Text>}
             {warning === "authFailed" && <Text style={styles.warningText}>비밀번호가 일치하지 않습니다. 다시 시도해주세요.</Text>}
@@ -238,10 +245,10 @@ const LoginForm = () => {
             )}
           </Pressable>
         </View>
-        
+
         <View style={styles.lowButtonContainer}>
-          <Pressable style={[styles.lowBtn, {borderLeftWidth: 0}]}
-             onPress={() => navigation.navigate('SignUp')}>
+          <Pressable style={[styles.lowBtn, { borderLeftWidth: 0 }]}
+            onPress={() => navigation.navigate('SignUp')}>
             <Text style={styles.lowBtnText}>회원 가입</Text>
           </Pressable>
           <Pressable style={styles.lowBtn} onPress={() => navigation.navigate('ForgotPassword')}>
@@ -259,81 +266,97 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "black",
   },
-  logoContainer : {
+  logoContainer: {
     height: windowHeight * 0.35,
     justifyContent: "center",
   },
-    logo: {
-      width: wp(60),
-      height: hp(30),
-      alignSelf: "center",
-    },
-  
+  logo: {
+    width: wp(60),
+    height: hp(30),
+    alignSelf: "center",
+  },
+
   bodyContainer: {
     height: windowHeight * 0.65,
     justifyContent: "center",
   },
-    inputContainer: {
-      height: windowHeight * 0.28,
-      alignItems: "center",
-    },
-      inputComposition: {
-        width: windowWidth * 0.85,
-        height: "50%",
-      },
-      inputText: {
-        fontSize: windowHeight * 0.02,
-        color: "#fff",
-        fontWeight: "bold",
-        paddingBottom: windowHeight * 0.02,
-      },
-      input: {
-        width: "100%",
-        paddingBottom: windowHeight * 0.005,
-        borderBottomWidth: 1,
-        borderColor: "#fff",
-        fontSize: windowHeight * 0.02,
-        color: "#fff",
-      },
-      warningText: {
-        color: "red",
-        fontSize: hp(1.8),
-        paddingTop: hp(1),
-      },
-    loginBtnContainer: {
-      height: windowHeight * 0.075,
-      justifyContent: "center",
-      alignSelf: "center",
-    },
-      loginBtn: {
-        width: windowWidth * 0.85,
-        height: windowHeight * 0.075,
-        borderRadius: 12,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      loginBtnText: {
-        fontSize: windowHeight * 0.02,
-        color: "#fff",
-        fontWeight: "bold",
-      },
-    lowButtonContainer: {
-      height: windowHeight * 0.3,
-      width: windowWidth,
-      flexDirection: "row",
-    },
-      lowBtnText: {
-        color: "#fff",
-        fontSize: hp(2),
-        textAlign: "center",
-      },
-      lowBtn: {
-        height: hp(3),
-        width: wp(50),
-        borderLeftWidth: 1,
-        borderColor: "#fff",
-        marginTop: hp(10),
-      },
+  inputContainer: {
+    height: windowHeight * 0.28,
+    alignItems: "center",
+  },
+  inputComposition: {
+    width: windowWidth * 0.85,
+    height: "50%",
+  },
+  inputText: {
+    fontSize: windowHeight * 0.02,
+    color: "#fff",
+    fontWeight: "bold",
+    paddingBottom: windowHeight * 0.02,
+  },
+  input: {
+    width: "100%",
+    paddingBottom: windowHeight * 0.005,
+    borderBottomWidth: 1,
+    borderColor: "#fff",
+    fontSize: windowHeight * 0.02,
+    color: "#fff",
+  },
+  passwordInput: {
+    flex: 1,  // Flex 사용하여 TextInput 확장
+    paddingBottom: windowHeight * 0.005,
+    fontSize: windowHeight * 0.02,
+    color: "#fff",
+  },
+  warningText: {
+    color: "red",
+    fontSize: hp(1.8),
+    paddingTop: hp(1),
+  },
+  loginBtnContainer: {
+    height: windowHeight * 0.075,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  loginBtn: {
+    width: windowWidth * 0.85,
+    height: windowHeight * 0.075,
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginBtnText: {
+    fontSize: windowHeight * 0.02,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  lowButtonContainer: {
+    height: windowHeight * 0.3,
+    width: windowWidth,
+    flexDirection: "row",
+  },
+  lowBtnText: {
+    color: "#fff",
+    fontSize: hp(2),
+    textAlign: "center",
+  },
+  lowBtn: {
+    height: hp(3),
+    width: wp(50),
+    borderLeftWidth: 1,
+    borderColor: "#fff",
+    marginTop: hp(10),
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: "#fff",
+  },
+  toggleButton: {
+    paddingHorizontal: 10,
+    
+  },
 });
 
 export default LoginForm;
