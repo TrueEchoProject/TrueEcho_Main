de2c4a1f48432470f89499b034606d837ce85dea
Huijun Seo <gjshj366@gmail.com>
2024-08-18 12:44:57 +0900

    chore : 배포용 json 파일 설정

--- src/main/java/te/trueEcho/infra/firebase/config/FirebaseConfig.java
+++ src/main/java/te/trueEcho/infra/firebase/config/FirebaseConfig.java
@@ -1,38 +1,40 @@
 package te.trueEcho.infra.firebase.config;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Value("${firebase-service-account-key}")
-    private String serviceAccountKeyString;
-
-
+    @Value("${firebase-service-account-key-path}")
+    private String serviceAccountKeyPath;
 
     @PostConstruct
     public void initialize() {
         try {
-            InputStream serviceAccount =
-                    new ByteArrayInputStream(serviceAccountKeyString.getBytes(StandardCharsets.UTF_8));
+            // JSON 파일 경로로부터 InputStream 생성
+            InputStream serviceAccount = new ClassPathResource(serviceAccountKeyPath).getInputStream();
 
+            // FirebaseOptions 빌드
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
+            // FirebaseApp이 초기화되지 않은 경우에만 초기화
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
             }
-        } catch (IOException e) {            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
